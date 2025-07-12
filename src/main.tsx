18b5393947b6e5c3e18bd33f0f792c232e99b68c
melentepan <stepanmelente@yandex.ru>
2025-07-12 19:27:37 +0300

    Change AppWrapper

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import styled from 'styled-components'
 const AppWrapper = styled.div`
   height: calc(100vh - 20px);
   max-width: 600px;
-  border-radius: 20px;
   padding: 10px 15px;
-  border: 3px solid #61afee;
+  border-left: 3px solid #61afee;
+  border-right: 3px solid #61afee;
   margin: 0 auto;
 `
 
