fe99774f5e5aa7801cda4492175bd5de67930122
melentepan <stepanmelente@yandex.ru>
2025-07-15 22:18:59 +0300

    Change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Flex } from 'antd'
 import AddTodo from './components/AddTodo/AddTodo'
+import TodoList from './components/TodoList/TodoList'
 
 function App() {
   return (
-    <>
+    <Flex vertical gap={50}>
       <AddTodo />
-    </>
+      <TodoList />
+    </Flex>
   )
 }
 
