ee2903669bf248768bb078d8386cc37b68d833dd
melentepan <stepanmelente@yandex.ru>
2025-07-16 22:17:32 +0300

    Update AppWrapper styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,11 @@ import styled from 'styled-components'
 const AppWrapper = styled.div`
   height: calc(100vh - 20px);
   max-width: 600px;
-  padding: 10px 15px;
-  border-left: 3px solid #61afee;
-  border-right: 3px solid #61afee;
+  padding: 0 15px;
+  border-left: 3px solid #1777ff;
+  border-right: 3px solid #1777ff;
   margin: 0 auto;
+  overflow: auto;
 `
 
 createRoot(document.getElementById('root')!).render(
