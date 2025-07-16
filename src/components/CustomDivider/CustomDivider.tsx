ece9905828a0be0131beb0c47e2c146898f23946
melentepan <stepanmelente@yandex.ru>
2025-07-16 22:18:34 +0300

    Update CustomDivider styles

--- src/components/CustomDivider/CustomDivider.tsx
+++ src/components/CustomDivider/CustomDivider.tsx
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 const CustomDivider = styled(Divider)`
   && {
     border-color: black;
+    position: sticky;
+    top: -10px;
+    background-color: white;
+    z-index: 10;
   }
 
   && .ant-divider-inner-text {
