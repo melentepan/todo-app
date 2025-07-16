1850699700b230ab3f5d9d74453b6bde855b0753
melentepan <stepanmelente@yandex.ru>
2025-07-16 22:15:09 +0300

    Add mock todos in App.tsx + add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Flex } from 'antd'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
+import { useState } from 'react'
+import type { Todo } from './types'
 
 function App() {
+  const [todosList, setTodosList] = useState<Todo[]>(
+    Array.from({ length: 15 }, (_, i) => ({
+      id: i + 1,
+      text: `Task #${i + 1}`,
+      completed: Math.random() < 0.5,
+      createdAt: new Date(Date.now() - Math.floor(Math.random() * 1000000000)),
+    }))
+  )
+
   return (
-    <Flex vertical gap={50}>
-      <AddTodo />
-      <TodoList />
+    <Flex vertical gap={15}>
+      <AddTodo setTodosList={setTodosList} />
+      <TodoList todosList={todosList} setTodosList={setTodosList} />
     </Flex>
   )
 }
