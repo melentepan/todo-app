733248bd4c7568a45927208af47c8c0cbc21a2df
melentepan <stepanmelente@yandex.ru>
2025-07-16 22:22:12 +0300

    Update AddTodo

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,20 +1,45 @@
 import { Input, Button, Flex } from 'antd'
 import CustomDivider from '../CustomDivider/CustomDivider'
+import type { Todo } from '../../types'
+import { useState } from 'react'
 
-const { TextArea } = Input
+interface AddTodoProps {
+  setTodosList: React.Dispatch<React.SetStateAction<Todo[]>>
+}
 
-export default function AddTodo() {
+export default function AddTodo({ setTodosList }: AddTodoProps) {
+  const [inputValue, setInputValue] = useState('')
+
+  function addTaskHandler() {
+    if (inputValue.trim() !== '') {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: inputValue,
+        completed: false,
+        createdAt: new Date(),
+      }
+
+      setTodosList((prev) => {
+        return [...prev, newTodo]
+      })
+
+      setInputValue('')
+    }
+  }
+
   return (
     <div>
       <CustomDivider>Add task</CustomDivider>
       <Flex vertical gap={10}>
-        <Input size='large' placeholder='Task title' />
-        <TextArea
+        <Input
           size='large'
-          placeholder='Task description'
-          autoSize={{ minRows: 1, maxRows: 3 }}
+          showCount
+          maxLength={50}
+          placeholder='Task'
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
         />
-        <Button size='large' type='primary'>
+        <Button size='large' type='primary' onClick={() => addTaskHandler()}>
           Add task
         </Button>
       </Flex>
