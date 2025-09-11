b610f7751f231a57a6a7cbd19a4aea3b08f18294
melentepan <stepanmelente@yandex.ru>
2025-09-12 01:04:59 +0300

    Fix APIs

--- src/api/privateApi.ts
+++ src/api/privateApi.ts
@@ -8,7 +8,6 @@ import axios from 'axios'
 
 export const privateApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
