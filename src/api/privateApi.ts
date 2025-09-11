617e1e377375db50e7f423783f01856842e98d03
melentepan <stepanmelente@yandex.ru>
2025-09-11 20:14:02 +0300

    Add Response interceptor on 401 code

--- src/api/privateApi.ts
+++ src/api/privateApi.ts
@@ -1,3 +1,9 @@
+import type { AuthTokens } from '@/types'
+import {
+  loadRefreshToken,
+  removeTokens,
+  saveTokens,
+} from '@/utils/localStorage'
 import axios from 'axios'
 
 export const privateApi = axios.create({
@@ -15,3 +21,38 @@ privateApi.interceptors.request.use((config) => {
   }
   return config
 })
+
+privateApi.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config
+
+    if (error.response?.status === 401 && !originalRequest._isRetry) {
+      originalRequest._isRetry = true
+      try {
+        const refreshToken = loadRefreshToken()
+        if (!refreshToken) {
+          removeTokens()
+          return Promise.reject(error)
+        }
+
+        const { data } = await axios.post<AuthTokens>(
+          `${import.meta.env.VITE_API_URL}/auth/refresh`,
+          {
+            refreshToken,
+          }
+        )
+
+        saveTokens(data.accessToken, data.refreshToken)
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`
+
+        return privateApi(originalRequest)
+      } catch (e) {
+        removeTokens()
+        return Promise.reject(e)
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
